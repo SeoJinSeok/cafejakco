f949b60334a8f5fd8f681a75f220661f0e4e39e7
Jinseok <alimm23@naver.com>
2012-09-13 15:52:01 +0900

    community와 membership URL 매핑
    GET,POST,DELETE방식만 구현

--- cafejakco/urls.py
+++ cafejakco/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from tastypie.api import Api
 from community.api import EntryResource
+from membership.api import EntryResource, UserResource
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,9 +9,12 @@ admin.autodiscover()
 
 v1_api = Api (api_name='community')
 v1_api.register(EntryResource())
+v2_api = Api (api_name='membership')
+v2_api.register(EntryResource())
+v2_api.register(UserResource())
 urlpatterns = patterns('',
     (r'^jakco/', include(v1_api.urls)),
-     
+    (r'^jakco/', include(v2_api.urls)),
     # Examples:
     # url(r'^$', 'cafejakco.views.home', name='home'),
     # url(r'^cafejakco/', include('cafejakco.foo.urls')),
--- membership/api.py
+++ membership/api.py
@@ -2,12 +2,19 @@ from django.contrib.auth.models import User
 from tastypie import fields
 from tastypie.authorization import DjangoAuthorization
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-from community.models import Entry
+from membership.models import Group
+
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'auth/user'
 
 class EntryResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
     class Meta:
-        queryset = Entry.objects.all()
-        resource_name = 'user'
+        queryset = Group.objects.all()
+        resource_name = 'group'
         authorization = DjangoAuthorization()
         list_allowed_methods =['get','post','delete']
         filtering = {
